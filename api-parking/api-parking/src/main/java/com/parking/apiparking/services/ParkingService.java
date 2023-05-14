3fb64a0e67bb5ac2f4d83b06ba4ab30ca2dbbf49
Sagredag <sagredag2@upao.edu.pe>
2023-05-14 18:08:42 -0500

    feat: Rellenar la clase ParkingService. Se implementó el método getAllCars

--- api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,4 +1,21 @@
 package com.parking.apiparking.services;
 
+import com.parking.apiparking.entities.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingService {
+    private List<Car> parkinglot;
+    public ParkingService(){
+        this.parkinglot = new ArrayList<>();
+    }
+
+    //TODO: Como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una visión general del estado actual del estacionamiento
+    public List<Car> getAllCars(){
+        return this.parkinglot;
+    }
+
+    
 }
